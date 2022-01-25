330ccb551f9e90f019a21b2e3ad584bf0ac7b6c0
Mohorima Islam <islammohorima@gmail.com>
2022-01-25 11:57:14 +0600

    added gd to composer.json

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -9,31 +9,6 @@
             @{{ $route.meta.title ? $route.meta.title : "BugTrack" }}
         </div>
 
-        <!-- Search Form -->
-        <form
-            class="
-                navbar-search navbar-search-dark
-                form-inline
-                mr-3
-                d-none d-md-flex
-                ml-lg-auto
-            "
-        >
-            <div class="form-group mb-0">
-                <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"
-                            ><i class="fas fa-search"></i
-                        ></span>
-                    </div>
-                    <input
-                        class="form-control"
-                        placeholder="Search"
-                        type="text"
-                    />
-                </div>
-            </div>
-        </form>
         <!-- User -->
         <ul class="navbar-nav align-items-center d-none d-md-flex">
             <li class="nav-item dropdown">
@@ -49,9 +24,7 @@
                         <span class="avatar avatar-sm rounded-circle">
                             <img
                                 alt="Image placeholder"
-                                src="{{
-                                    asset('argon')
-                                }}/img/theme/team-4-800x800.jpg"
+                                src="/images/lavisco/img-bg.jpg"
                             />
                         </span>
                         <div class="media-body ml-2 d-none d-lg-block">

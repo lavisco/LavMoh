a9f3e964a2999be81568d8eb14965ff64acd0eb8
Mohorima Islam <islammohorima@gmail.com>
2022-03-17 02:57:24 +0600

    .

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -60,35 +60,9 @@
                     </div>
                     <!-- Swiper end -->
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 input-form">
                     <div class="row">
-                        <h3 class="text-left col-9">{{ product.title }}</h3>
-                        <div class="col-3 text-right">
-                            <svg
-                                width="34"
-                                height="34"
-                                viewBox="0 0 45 45"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M40.5615 12.4632C39.9725 11.0993 39.1231 9.86331 38.061 8.82449C36.9981 7.78258 35.745 6.95458 34.3696 6.38553C32.9435 5.79312 31.4139 5.48988 29.8696 5.49344C27.7031 5.49344 25.5894 6.0867 23.7524 7.20731C23.313 7.47537 22.8955 7.76981 22.5 8.09061C22.1045 7.76981 21.687 7.47537 21.2476 7.20731C19.4106 6.0867 17.2969 5.49344 15.1304 5.49344C13.5703 5.49344 12.0586 5.79227 10.6304 6.38553C9.25049 6.95682 8.00684 7.7786 6.93896 8.82449C5.87547 9.86214 5.02593 11.0984 4.43848 12.4632C3.82764 13.8826 3.51562 15.3899 3.51562 16.9412C3.51562 18.4046 3.81445 19.9295 4.40771 21.4807C4.9043 22.7771 5.61621 24.1219 6.52588 25.4798C7.96729 27.6287 9.94922 29.8699 12.4102 32.1419C16.4883 35.908 20.5269 38.5096 20.6982 38.615L21.7397 39.283C22.2012 39.5774 22.7944 39.5774 23.2559 39.283L24.2974 38.615C24.4688 38.5052 28.5029 35.908 32.5854 32.1419C35.0464 29.8699 37.0283 27.6287 38.4697 25.4798C39.3794 24.1219 40.0957 22.7771 40.5879 21.4807C41.1812 19.9295 41.48 18.4046 41.48 16.9412C41.4844 15.3899 41.1724 13.8826 40.5615 12.4632ZM22.5 35.8069C22.5 35.8069 6.85547 25.783 6.85547 16.9412C6.85547 12.4632 10.5601 8.83328 15.1304 8.83328C18.3428 8.83328 21.1289 10.6263 22.5 13.2454C23.8711 10.6263 26.6572 8.83328 29.8696 8.83328C34.4399 8.83328 38.1445 12.4632 38.1445 16.9412C38.1445 25.783 22.5 35.8069 22.5 35.8069Z"
-                                    fill="#333333"
-                                />
-                            </svg>
-                            <svg
-                                width="34"
-                                height="34"
-                                viewBox="0 0 45 45"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M5.625 22.5C5.625 25.6012 8.14875 28.125 11.25 28.125C12.7387 28.125 14.085 27.5344 15.0919 26.5875L26.325 33.0075C26.2913 33.2512 26.25 33.495 26.25 33.75C26.25 36.8512 28.7738 39.375 31.875 39.375C34.9762 39.375 37.5 36.8512 37.5 33.75C37.5 30.6488 34.9762 28.125 31.875 28.125C30.3862 28.125 29.04 28.7156 28.0331 29.6625L16.8 23.2444C16.8337 22.9988 16.875 22.755 16.875 22.5C16.875 22.245 16.8337 22.0012 16.8 21.7556L28.0331 15.3375C29.04 16.2844 30.3862 16.875 31.875 16.875C34.9762 16.875 37.5 14.3512 37.5 11.25C37.5 8.14875 34.9762 5.625 31.875 5.625C28.7738 5.625 26.25 8.14875 26.25 11.25C26.25 11.505 26.2913 11.7487 26.325 11.9944L15.0919 18.4125C14.055 17.4275 12.6801 16.8773 11.25 16.875C8.14875 16.875 5.625 19.3988 5.625 22.5Z"
-                                    fill="black"
-                                />
-                            </svg>
-                        </div>
+                        <h3 class="text-left col-12">{{ product.title }}</h3>
                     </div>
                     <div
                         class="
@@ -110,73 +84,25 @@
                                 >Made to Order</span
                             >
                         </div>
-                        <div class="d-flex align-items-center">
-                            <div class="rating mr-2">
-                                <svg
-                                    width="24"
-                                    height="22"
-                                    viewBox="0 0 24 22"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M12 0L14.6942 8.2918H23.4127L16.3593 13.4164L19.0534 21.7082L12 16.5836L4.94658 21.7082L7.64074 13.4164L0.587322 8.2918H9.30583L12 0Z"
-                                        fill="#C4C4C4"
-                                    />
-                                </svg>
-                                <svg
-                                    width="24"
-                                    height="22"
-                                    viewBox="0 0 24 22"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M12 0L14.6942 8.2918H23.4127L16.3593 13.4164L19.0534 21.7082L12 16.5836L4.94658 21.7082L7.64074 13.4164L0.587322 8.2918H9.30583L12 0Z"
-                                        fill="#C4C4C4"
-                                    />
-                                </svg>
-                                <svg
-                                    width="24"
-                                    height="22"
-                                    viewBox="0 0 24 22"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M12 0L14.6942 8.2918H23.4127L16.3593 13.4164L19.0534 21.7082L12 16.5836L4.94658 21.7082L7.64074 13.4164L0.587322 8.2918H9.30583L12 0Z"
-                                        fill="#C4C4C4"
-                                    />
-                                </svg>
-                                <svg
-                                    width="24"
-                                    height="22"
-                                    viewBox="0 0 24 22"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M12 0L14.6942 8.2918H23.4127L16.3593 13.4164L19.0534 21.7082L12 16.5836L4.94658 21.7082L7.64074 13.4164L0.587322 8.2918H9.30583L12 0Z"
-                                        fill="#C4C4C4"
-                                    />
-                                </svg>
-                            </div>
-                            <h4 class="mb-0">4.2</h4>
-                        </div>
                     </div>
                     <div
                         class="mt-4"
                         v-show="variations"
-                        v-for="variation in variations"
+                        v-for="(variation, index) in variations"
+                        :key="variation.id"
                     >
                         <h6>{{ variation[0].type }}</h6>
 
-                        <select class="custom-select website-select col-md-6">
+                        <select
+                            class="custom-select col-md-6"
+                            @change="recordVariationDetails($event, variation)"
+                        >
                             <option value="" disabled selected hidden>
                                 Select {{ variation[0].type }}
                             </option>
-                            <option v-for="option in variation">
+                            <option v-for="(option, i) in variation" :key="option.id" :value="i">
                                 {{ option.type_option }}
+                                + {{ option.price }} lkr
                             </option>
                         </select>
                     </div>
@@ -188,7 +114,7 @@
                         <h6>About this item</h6>
                         <p>{{ product.short_description }}</p>
                     </div>
-                    <div class="mt-4" v-show="has_custom_text">
+                    <div class="mt-4" v-show="product.has_custom_text == 1">
                         <h6>Custom message</h6>
 
                         <input
@@ -304,14 +230,17 @@
 
                         <div class="collapse" id="collapseFAQ">
                             <div class="card faq-card">
-                                <p>Q: Question</p>
-                                <p>A: Answer</p>
+                                <p>How is the pricing calculated?</p>
+                                <p>
+                                    The price of a product consists of its base
+                                    price + variation price
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div class="mt-4">
                         <hr />
-                        <h6>Meet your Seller</h6>
+                        <h6 class="mt-4">Meet your Seller</h6>
                         <div class="d-flex">
                             <img
                                 src="/images/lavisco/img-bg.jpg"
@@ -369,10 +298,6 @@ export default {
     data: () => ({
         product: [],
         loading: true,
-        form: new Form({
-            id: "",
-            custom_text: "",
-        }),
         swiperOptionTop: {
             loop: true,
             loopedSlides: 3, // looped slides should be the same
@@ -391,7 +316,35 @@ export default {
             touchRatio: 0.2,
             slideToClickedSlide: true,
         },
+        form: new Form({
+            id: "",
+            custom_text: "",
+            variation_type_option: [null, null, null],
+            selected_variations: [
+                {
+                    variation_type_option: "",
+                    variation_price: "",
+                },
+                {
+                    variation_type_option: "",
+                    variation_price: "",
+                },
+                {
+                    variation_type_option: "",
+                    variation_price: "",
+                },
+            ],
+        }),
     }),
+
+    computed: {
+        variations() {
+            return _.groupBy(
+                this.product.product_variations,
+                (variation) => variation.type
+            );
+        },
+    },
 
     beforeRouteEnter: function (to, from, next) {
         let uri = "/api/products/" + to.params.productId;
@@ -411,20 +364,24 @@ export default {
         });
     },
 
-    computed: {
-        variations() {
-            return _.groupBy(
-                this.product.product_variations,
-                (variation) => variation.type
-            );
-        },
-    },
-
     methods: {
         setData(response) {
             this.product = response.data;
             this.loading = false;
         },
+
+        recordVariationDetails(event, variation) {
+            //console.log(option);
+            let index = event.target.value;
+            console.log(variation[index].type_option);
+            console.log(variation[index].price);
+
+            this.$set( this.form.selected_variations[index], 'variation_type_option', variation[index].type_option);
+
+            //this.form.selected_variations[index].variation_type_option = variation[index].type_option;
+            this.form.selected_variations[index].variation_price = variation[index].price;
+        },
+
         loadProduct() {
             axios
                 .get("/api/products/" + this.$route.params.productId)
@@ -434,6 +391,7 @@ export default {
                 })
                 .catch((error) => console.log(error));
         },
+
         addProductToCart(product) {
             this.$store.dispatch("addProductToCart", product);
         },

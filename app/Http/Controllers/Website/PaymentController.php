52065d19fe3e823f10a424e9209299e16d758872
Mohorima Islam <islammohorima@gmail.com>
2022-08-17 14:50:11 +0600

    .

--- app/Http/Controllers/Website/PaymentController.php
+++ app/Http/Controllers/Website/PaymentController.php
@@ -260,38 +260,38 @@ pJ28AUyd0dWx1YWu1wIDAQAB
     public function paymentResponse(Request $request)
     {
         //decode & get POST parameters
-        // $payment = base64_decode($_POST ["payment"]);
-        // $signature = base64_decode($_POST ["signature"]);
-        // //load public key for signature matching
-        // $publickey = "-----BEGIN PUBLIC KEY-----
-        // MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6nVc/ykIWsT1ktI8/49nfBUOQ
-        // IHCCu9bC+pxEYbPvUth28MWitvm7y2u2nX/3/nVXMdvl2yiAbB7aBw4dGnAhXoAM
-        // 9WB8nw3AZS1VGqBBEKFs23EqUvjsBxrj+QasVkeZwC+oxvGuuprCIFW9o7w290c0
-        // pJ28AUyd0dWx1YWu1wIDAQAB
-        // -----END PUBLIC KEY-----";
-        // openssl_public_decrypt($signature, $value, $publickey);
+        $payment = base64_decode($_POST ["payment"]);
+        $signature = base64_decode($_POST ["signature"]);
+        //load public key for signature matching
+        $publickey = "-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6nVc/ykIWsT1ktI8/49nfBUOQ
+IHCCu9bC+pxEYbPvUth28MWitvm7y2u2nX/3/nVXMdvl2yiAbB7aBw4dGnAhXoAM
+9WB8nw3AZS1VGqBBEKFs23EqUvjsBxrj+QasVkeZwC+oxvGuuprCIFW9o7w290c0
+pJ28AUyd0dWx1YWu1wIDAQAB
+-----END PUBLIC KEY-----";
+        openssl_public_decrypt($signature, $value, $publickey);
 
-        // $signature_status = false ;
+        $signature_status = false ;
 
-        // if($value == $payment)
-        // {
-        //     $signature_status = true ;
-        // }
+        if($value == $payment)
+        {
+            $signature_status = true ;
+        }
 
-        // //get payment response in segments
-        // //payment format: order_id|order_refference_number|date_time_transaction|payment_gateway_used|status_code|comment;
-        // $responseVariables = explode('|', $payment);      
+        //get payment response in segments
+        //payment format: order_id|order_refference_number|date_time_transaction|payment_gateway_used|status_code|comment;
+        $responseVariables = explode('|', $payment);      
 
-        // if($signature_status == true)
-        // {
-        //     //display values
-        //     echo $signature_status;
-        //     echo '<br/>';
-        //     var_dump($responseVariables);
-        // } else
-        // {
-        //     echo 'Error Validation'; 
-        // }
+        if($signature_status == true)
+        {
+            //display values
+            echo $signature_status;
+            echo '<br/>';
+            var_dump($responseVariables);
+        } else
+        {
+            echo 'Error Validation'; 
+        }
 
         echo $request;
     }

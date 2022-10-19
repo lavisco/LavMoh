38983d0e683817afbe6a64e26cb2fc6281d72d1c
Mohorima Islam <islammohorima@gmail.com>
2022-10-19 16:45:55 +0600

    .

--- app/Http/Controllers/Website/ExcelController.php
+++ app/Http/Controllers/Website/ExcelController.php
@@ -11,7 +11,7 @@ use App\Exports\UsersExport;
 use App\Exports\UsersSellerExport;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
+use Excel;
 
 class ExcelController extends Controller
 {
